edbf820c916bbb5ac606acd04fdf07fb45fef13e
Chris Ober <6597906+Quyzi@users.noreply.github.com>
2024-08-20 02:02:39 -0400

    proper serde

--- backend/src/objects.rs
+++ backend/src/objects.rs
@@ -1,7 +1,6 @@
-use std::fmt::Display;
-
 use serde::{Deserialize, Serialize};
 use sled::IVec;
+use std::fmt::Display;
 
 use crate::errors::MauveError;
 
@@ -33,5 +32,53 @@ impl TryFrom<(IVec, IVec)> for ObjectRef {
         let collection = String::from_utf8(collection.to_vec())?;
         let name = String::from_utf8(name.to_vec())?;
         Ok(Self { name, collection })
+    }
+}
+
+pub trait ToFromMauve<T: Serialize + for<'de> Deserialize<'de>> {
+    fn to_object(&self) -> Result<Vec<u8>, MauveError>;
+    fn from_object(b: Vec<u8>) -> Result<T, MauveError>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ToFromMauve;
+    use crate::errors::MauveError;
+    use macros::MauveObject;
+    use rand::{thread_rng, Rng, RngCore};
+    use serde::{Deserialize, Serialize};
+    use std::io::BufReader;
+
+    #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, MauveObject)]
+    struct TestObject {
+        onekb: Vec<u8>,
+        s: String,
+        x: u128,
+    }
+
+    impl TestObject {
+        fn rand() -> Self {
+            let mut rng = thread_rng();
+            let mut buf = [0u8; 1024];
+            rng.fill_bytes(&mut buf);
+            assert_ne!([0u8; 1024], buf);
+            Self {
+                onekb: buf.to_vec(),
+                s: format!("{}", rng.gen_range(1..u128::MAX)),
+                x: rng.gen(),
+            }
+        }
+    }
+
+    #[test]
+    fn test_mauve_object() -> anyhow::Result<()> {
+        for n in 0..10 {
+            let object = TestObject::rand();
+            let bytes = object.to_object()?;
+            let got = TestObject::from_object(bytes)?;
+            assert_eq!(object, got);
+            println!("test {n} is ok");
+        }
+        Ok(())
     }
 }
